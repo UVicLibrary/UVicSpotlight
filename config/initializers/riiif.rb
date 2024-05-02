16b24e6381ddd573ead55cd0a64dea43fbbb6819
eltiffster <tjychan@uvic.ca>
2024-05-02 13:13:35 -0700

    Use graphicsmagick to serve IIIF images in production.

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -7,4 +7,9 @@ ActiveSupport::Reloader.to_prepare do
   # Riiif.not_found_image = 'app/assets/images/us_404.svg'
   #
   Riiif::Engine.config.cache_duration = 365.days
+
+  if Rails.env.production?
+    Riiif::ImagemagickCommandFactory.external_command = "gm convert"
+    Riiif::ImageMagickInfoExtractor.external_command = "gm identify"
+  end
 end
