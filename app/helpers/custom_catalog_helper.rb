2a0f5c3677ac7742aa71c85ed6da333f8aa1c9f8
Tiffany Chan <tjychan@uvic.ca>
2024-06-20 15:06:37 -0700

    Fix problematic regex that was causing the bug in a1d5b87ece8d3ba35b9c7280fc5d9723b550a1fb.

--- app/helpers/custom_catalog_helper.rb
+++ app/helpers/custom_catalog_helper.rb
@@ -24,11 +24,11 @@ module CustomCatalogHelper
   def field_value(presenter, field)
     value = presenter.field_value(field)
     if multiple?(value)
-      sanitize(value.split(/ ?; ?/).map do |val|
-        val.start_with?('http') ? sanitize(render_link_to(val)) : Nokogiri::XML.fragment(sanitize(scrub_value(val)))
+      sanitize(value.split(/\s; \s/).map do |val|
+        val.start_with?('http') ? render_link_to(val) : scrub_value(val)
       end.join('; '))
     else
-      value.start_with?('http') ? sanitize(render_link_to(value)) : Nokogiri::XML.fragment(sanitize(scrub_value(value)))
+      value.start_with?('http') ? sanitize(render_link_to(value)) : sanitize(scrub_value(value))
     end
   end
 
