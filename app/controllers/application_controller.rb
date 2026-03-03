b48b2e400ac35b330f8e772543619f074f6391ac
eltiffster <tjychan@uvic.ca>
2026-03-03 14:04:41 -0800

    Patch ApplicationController so it works with load balancer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
   # skip_after_action :discard_flash_if_xhr
   layout 'blacklight'
 
+  # Patch for load balancer: use HTTPS urls even when config.use_ssl is false
+  def default_url_options(options={})
+    Rails.env.production? ? options.merge(protocol: :https) : options
+  end
+
+
 end
