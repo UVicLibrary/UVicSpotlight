1cd3f475e89ac4b3d2dd3845a32ddb16dc7236a6
Ethan Getty <35879251+EGetty@users.noreply.github.com>
2026-03-03 16:17:03 -0800

    Updates to javascript
    Match requirements to spotlight-4.7.1 so javascript files are properly loaded

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,9 @@
 // Required by Blacklight
 //= require popper
 // Twitter Typeahead for autocomplete
+//= link_tree ../../../vendor/assets
+//= require leaflet
+//= require clipboard
 //= require twitter/typeahead
 //= require bootstrap
 //= require blacklight/blacklight
@@ -11,4 +14,10 @@
 //= require sortable-rails-jquery
 //= require turbolinks
 //= require markerclusterer
-//= catalog_show
+//= require sir-trevor
+// require spotlight/sir_trevor/blocks/footnotes.js
+// require spotlight/sir_trevor/blocks/google_map.js
+//= require tiny-slider
+//= require typeahead.bundle.min.js
+//= require catalog_show
+//= require spotlight/spotlight
