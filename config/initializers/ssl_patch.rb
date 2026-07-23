932d7efbc93548c750a3ba11b012c9bc49bfe93f
eltiffster <tjychan@uvic.ca>
2026-07-23 15:54:37 -0700

    Add conditional to config/initializers/ssl_patch.rb

--- config/initializers/ssl_patch.rb
+++ config/initializers/ssl_patch.rb
@@ -1,5 +1,7 @@
-# iGNORE unexpected EOF error with Vault's SSL cert and OpenSSL gem v.3+
+# Ignore unexpected EOF error with Vault's SSL cert and OpenSSL gem v.3+
 # https://stackoverflow.com/questions/76183622/since-a-ruby-container-upgrade-we-expirience-a-lot-of-opensslsslsslerror
-OpenSSL::SSL::SSLContext::DEFAULT_PARAMS = OpenSSL::SSL::SSLContext::DEFAULT_PARAMS.merge(
-  options: OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:options] | OpenSSL::SSL::OP_IGNORE_UNEXPECTED_EOF
-).freeze
+if OpenSSL::SSL.const_defined?(:OP_IGNORE_UNEXPECTED_EOF)
+  OpenSSL::SSL::SSLContext::DEFAULT_PARAMS = OpenSSL::SSL::SSLContext::DEFAULT_PARAMS.merge(
+    options: OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:options] |= OpenSSL::SSL::OP_IGNORE_UNEXPECTED_EOF
+  ).freeze
+end
